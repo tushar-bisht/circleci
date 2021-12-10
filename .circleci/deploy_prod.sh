9267878eddd71a1ebb6c0cb5c1c9108f819fd120
Ubuntu <ubuntu@ip-172-31-13-65.ap-south-1.compute.internal>
2021-12-10 08:57:28 +0000

    add file

--- .circleci/deploy_prod.sh
+++ .circleci/deploy_prod.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh ubuntu@65.1.93.116 "cd /var/www/html/production && git pull origin prod"
+ssh ubuntu@65.1.93.116 "cd /var/www/html/production && git pull origin prod && git config pull.ff only"
